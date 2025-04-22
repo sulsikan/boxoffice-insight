70e871e140c2ec70f2051384f3e22cf2718951a6
MochaSyrup <yeanjun0222@gmail.com>
2025-04-22 16:51:21 +0900

    feat: 장르별 영화 수에 따른 파이차트(2014~2025)

--- genre_trend/views.py
+++ genre_trend/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from genre_trend.models import MovieBasicInfo
+
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request,'base.html')
+def genre_stat(request):
+
+    movies = MovieBasicInfo.objects.all()
+    data = {}
+    for movie in movies :
+        genres = movie.genre.split(",")
+        for genre in genres :
+            genre = genre.strip()
+            data[genre] = data.get(genre, 0) +1
+    context = {
+        'labels': list(data.keys()),
+        'values': list(data.values()),
+    }
+    return render(request,'genre_stat.html', context)
