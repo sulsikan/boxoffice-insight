70776536f92b521e1d7d09af6f63bb15dc06cc7d
growingpsy <psy915@swu.ac.kr>
2025-04-23 17:01:45 +0900

    feat: html 페이지 커스터마이징 및 폰트 적용

--- regional_cumulative_stats/urls.py
+++ regional_cumulative_stats/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('regional_cumulative/', views.regional_cumulative, name='regional_cumulative')
-    ]
+    path('regional_cumulative/', views.regional_cumulative, name='regional_cumulative'),
+    path('api/movie_stats/', views.get_movie_stats, name='movie_stats'),
+    path('api/top_movies_by_region/', views.get_top_movies_by_region, name='top_movies_by_region'),
+]
