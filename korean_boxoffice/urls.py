973f8ac9ccdbb374603fdb499209a5d4d2dd9a90
SeonghyunJo <shaqbari@gmail.com>
2025-04-23 16:47:45 +0900

    월간, 연간 박스오피스 추가 파이차트에 점유율 5프로 이상만 표시되게 추가 대시보드 클릭시 월간 박스오피스 나오게 추가

--- korean_boxoffice/urls.py
+++ korean_boxoffice/urls.py
@@ -5,6 +5,8 @@ app_name = 'korean_boxoffice'
 urlpatterns = [
     path('', views.daily_boxoffice, name='index'),
     path('daily_boxoffice/', views.daily_boxoffice, name='daily_boxoffice'),
+    path('monthly_boxoffice/', views.monthly_boxoffice, name='monthly_boxoffice'),
+    path('annual_boxoffice/', views.annual_boxoffice, name='annual_boxoffice'),
     path('<int:pk>/', views.movie_info, name='movie_info'),
 
 ]
--- korean_boxoffice/views.py
+++ korean_boxoffice/views.py
@@ -3,7 +3,7 @@ from django.http import Http404, HttpResponseBadRequest, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
 from rest_framework.status import HTTP_400_BAD_REQUEST
 
-from korean_boxoffice.models import DailyBoxoffice, MovieInfo
+from korean_boxoffice.models import DailyBoxoffice, MovieInfo, MonthlyBoxoffice, AnnualBoxoffice
 
 # Create your views here.
 KST = timezone(timedelta(hours=+9))
@@ -25,25 +25,98 @@ def daily_boxoffice(request):
         print(e)
         return HttpResponseBadRequest('부적합한 요청 입니다.')
 
-    daily_boxoffices = DailyBoxoffice.objects.filter(ranking_date=target_dt)
-    if not daily_boxoffices:
+    boxoffices = DailyBoxoffice.objects.filter(ranking_date=target_dt).order_by('rank')[:10]
+    if not boxoffices:
         return HttpResponseNotFound('해당 데이터가 없습니다.')
         # raise Http404('데이터가 없습니다.')
 
-    percent_total = sum(db.revenue_share for db in daily_boxoffices)
+    percent_total = sum(db.revenue_share for db in boxoffices)
     etc_share = 100 - percent_total
 
     context = {
         'target_dt': target_dt,
         'target_date': f'{year:04d}-{month:02d}-{day:02d}',
-        'daily_boxoffices': daily_boxoffices,
-        'etc_share': etc_share
+        'boxoffices': boxoffices,
+        'etc_share': round(etc_share, 3)
     }
 
     return render(request, 'korean_boxoffice/daily_boxoffice.html', context=context)
 
 
+def monthly_boxoffice(request):
+    yesterday = datetime.now(KST) - timedelta(days=1)
+    target_date = request.GET.get('target_date',
+                                  f'{yesterday.year:04d}-{yesterday.month:02d}')  # 날자 정보 없으면 어제날자로
+
+    try:
+        str_year, str_month = target_date.split('-')
+        year = int(str_year)
+        month = int(str_month)
+        day = 1
+        target_dt = datetime(year=year, month=month, day=day, tzinfo=KST)
+    except Exception as e:
+        print(e)
+        return HttpResponseBadRequest('부적합한 요청 입니다.')
+
+    target_date = f'{year:04d}-{month:02d}'
+    boxoffices = MonthlyBoxoffice.objects.filter(ranking_date=target_date).order_by('rank')[:10]
+    if not boxoffices:
+        return HttpResponseNotFound('해당 데이터가 없습니다.')
+        # raise Http404('데이터가 없습니다.')
+
+    percent_total = sum(db.revenue_share for db in boxoffices)
+    etc_share = 100 - percent_total
+
+    select_dates = sorted(set(MonthlyBoxoffice.objects.values_list('ranking_date', flat=True)), reverse=True)[:24]
+    context = {
+        'target_dt':target_dt,
+        'target_date': target_date,
+        'select_dates': select_dates,
+        'boxoffices': boxoffices,
+        'etc_share': round(etc_share, 3)
+    }
+
+    return render(request, 'korean_boxoffice/monthly_boxoffice.html', context=context)
+
+
+def annual_boxoffice(request):
+    yesterday = datetime.now(KST) - timedelta(days=1)
+    target_date = request.GET.get('target_date',
+                                  f'{yesterday.year:04d}')  # 날자 정보 없으면 어제날자로
+
+    try:
+        year = int(target_date)
+        target_dt = datetime(year=year, month=1, day=1, tzinfo=KST)
+    except Exception as e:
+        print(e)
+        return HttpResponseBadRequest('부적합한 요청 입니다.')
+
+    boxoffices = AnnualBoxoffice.objects.filter(ranking_date=target_date).order_by('rank')[:10]
+    if not boxoffices:
+        return HttpResponseNotFound('해당 데이터가 없습니다.')
+        # raise Http404('데이터가 없습니다.')
+
+    percent_total = sum(db.revenue_share for db in boxoffices)
+    etc_share = 100 - percent_total
+
+    select_dates = [str(i) for i in range(yesterday.year, 2010 - 1, -1)]
+
+
+    context = {
+        'target_dt': target_dt,
+        'target_date': target_date,
+        'select_dates': select_dates,
+        'boxoffices': boxoffices,
+        'etc_share': round(etc_share, 3)
+    }
+
+    return render(request, 'korean_boxoffice/annual_boxoffice.html', context=context)
+
 def movie_info(request, pk):
     movie_info = get_object_or_404(MovieInfo, pk=pk)
+    context = {
+        'movie_info': movie_info,
+        'release_date': movie_info.release_date.astimezone(KST) if movie_info.release_date else None   ,
+    }
 
-    return render(request, 'korean_boxoffice/movie_info.html', {'movie_info': movie_info})
+    return render(request, 'korean_boxoffice/movie_info.html', context=context)
