9e7043ad8ed1ad5d137e32977e27acacc296a952
SeonghyunJo <shaqbari@gmail.com>
2025-04-23 10:42:00 +0900

    간단한 시각화 부트스트랩에 추가

--- korean_boxoffice/urls.py
+++ korean_boxoffice/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'korean_boxoffice'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('daily_boxoffice/<int:target_date>', views.daily_boxoffice, name='daily_boxoffice'), #YYYYMMDD
+    path('', views.daily_boxoffice, name='index'),
+    path('daily_boxoffice/', views.daily_boxoffice, name='daily_boxoffice'),
+    path('<int:pk>/', views.movie_info, name='movie_info'),
 
 ]
--- korean_boxoffice/views.py
+++ korean_boxoffice/views.py
@@ -1,41 +1,50 @@
 from datetime import datetime, timezone, timedelta
 
-from IPython.core.usage import default_banner
-from django.shortcuts import render
+from django.http import Http404, HttpResponseBadRequest, HttpResponseNotFound
+from django.shortcuts import render, get_object_or_404
+from rest_framework.status import HTTP_400_BAD_REQUEST
 
-from korean_boxoffice.models import DailyBoxoffice
+from korean_boxoffice.models import DailyBoxoffice, MovieInfo
 
 # Create your views here.
 KST = timezone(timedelta(hours=+9))
-default_dt = datetime(2025, 4, 16, tzinfo=KST)
 
 
 def index(request):
-    target_dt = f'{default_dt.year:04d}{default_dt.month:02d}{default_dt.day:02d}'
-    context = {
-        'year': default_dt.year,
-        'target_date': int(target_dt),
-    }
-    print(target_dt)
-    return render(request, 'korean_boxoffice/index.html', context=context)
+    return render(request, 'korean_boxoffice/index.html')
 
 
-def daily_boxoffice(request, target_date: int):
-    str_target_date = str(target_date)
-    year = int(str_target_date[:4])
-    month = int(str_target_date[4:6])
-    day = int(str_target_date[6:])
-    target_dt = datetime(year=year, month=month, day=day, tzinfo=KST)
+def daily_boxoffice(request):
+    yesterday = datetime.now(KST) - timedelta(days=1)
+    target_date = request.GET.get('target_date',
+                                  f'{yesterday.year:04d}-{yesterday.month:02d}-{yesterday.day:02d}')  # 날자 정보 없으면 어제날자로
 
-    # target_ranking_date = datetime(year, month, day, tzinfo=KST)
-    # print(target_ranking_date)
+    try:
+        year, month, day = [int(token) for token in target_date.split('-')]
+        target_dt = datetime(year=year, month=month, day=day, tzinfo=KST)
+    except Exception as e:
+        print(e)
+        return HttpResponseBadRequest('부적합한 요청 입니다.')
+
     daily_boxoffices = DailyBoxoffice.objects.filter(ranking_date=target_dt)
-    print(daily_boxoffices)
-    context = {
-        'my_text': 'hello world',
-        'target_ranking_date': target_dt,
-        'daily_boxoffices': daily_boxoffices
+    if not daily_boxoffices:
+        return HttpResponseNotFound('해당 데이터가 없습니다.')
+        # raise Http404('데이터가 없습니다.')
 
+    percent_total = sum(db.revenue_share for db in daily_boxoffices)
+    etc_share = 100 - percent_total
+
+    context = {
+        'target_dt': target_dt,
+        'target_date': f'{year:04d}-{month:02d}-{day:02d}',
+        'daily_boxoffices': daily_boxoffices,
+        'etc_share': etc_share
     }
 
     return render(request, 'korean_boxoffice/daily_boxoffice.html', context=context)
+
+
+def movie_info(request, pk):
+    movie_info = get_object_or_404(MovieInfo, pk=pk)
+
+    return render(request, 'korean_boxoffice/movie_info.html', {'movie_info': movie_info})
